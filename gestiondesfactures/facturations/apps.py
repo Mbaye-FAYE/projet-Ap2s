3a890f461d080d16c2b37d1d06295d555d2f3aa2
Mbaye <faye86712@gmail.com>
2025-07-16 17:18:43 +0000

    Fix: correct INSTALLED_APPS with full path

--- gestiondesfactures/facturations/apps.py
+++ gestiondesfactures/facturations/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class FacturationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'facturations'
+    name = 'gestiondesfactures.facturations'
 
     def ready(self):
         import facturations.signals
