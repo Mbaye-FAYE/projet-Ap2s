38fb212e56a2a265aca3e76d4c3c28201c3cf5e9
Mbaye <faye86712@gmail.com>
2025-07-17 16:17:30 +0000

    Réorganisation du projet : suppression du dossier racine inutile

--- gestiondesfactures/settings.py
+++ gestiondesfactures/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestiondesfactures.facturations',  # Ajout de notre application
+    'facturations',  # Ajout de notre application
     'rest_framework',
 ]
 
